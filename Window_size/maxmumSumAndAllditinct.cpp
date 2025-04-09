5cfa70c181bee53d45bffbc4a9c4886c9d6dbf56
Anurag Jain <jainanurag7nov@gmail.com>
2025-04-09 17:43:46 +0530

    maximumsum and all Distinct

---
+++ Window_size/maxmumSumAndAllditinct.cpp
@@ -0,0 +1,17 @@
+// find the subarray with maximum sum and all distinct elements
+
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, k ;
+    cin>>n>>k ;
+    vector<int> v(n);
+    for(auto & i : v){
+        cin>>i;
+    }
+
+    
+
+    return 0;
+}
