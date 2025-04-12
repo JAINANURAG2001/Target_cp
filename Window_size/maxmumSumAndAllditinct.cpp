d5fc0236642d55454666271a6a6ae16b9669e4aa
Anurag Jain <jainanurag7nov@gmail.com>
2025-04-12 15:49:58 +0530

    logic of maximumSum and All Distinct

--- Window_size/maxmumSumAndAllditinct.cpp
+++ Window_size/maxmumSumAndAllditinct.cpp
@@ -1,17 +1,55 @@
 // find the subarray with maximum sum and all distinct elements
 
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, k ;
-    cin>>n>>k ;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
     vector<int> v(n);
-    for(auto & i : v){
-        cin>>i;
+    for (auto &i : v)
+    {
+        cin >> i;
+    }
+    int ans = INT_MIN;
+    map<int, int> mp;
+    int sum = 0;
+    for (int i = 0; i < k; i++)
+    {
+        mp[v[i]]++;
+        sum += v[i];
     }
 
-    
+    if (mp.size() == k)
+    {
+        ans = max(ans, sum);
+    }
 
+    for (int i = k; i < n; i++)
+    {
+
+        mp[v[i]]++;
+        sum += v[i];
+        sum -= v[i - k];
+        mp[v[i - k]]--;
+
+        if (mp[v[i - k]] == 0)
+            mp.erase(v[i - k]);
+
+        if (mp.size() == k)
+        {
+            ans = max(ans, sum);
+        }
+    }
+
+    if (ans == INT_MIN)
+    {
+        cout << "Not possibe" << endl;
+    }
+    else
+    {
+        cout << ans << endl;
+    }
     return 0;
 }
