cd8d46cd6203a22b3dab2f00618583d91fbca899
Anurag Jain <jainanurag7nov@gmail.com>
2025-04-13 17:08:38 +0530

    index of Min element

---
+++ Window_size/indexOfMinimumElement.cpp
@@ -0,0 +1,23 @@
+// find the index of the minumum element of the subarray of size k 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (auto &i : v)
+    {
+        cin >> i;
+    }
+
+    // method 1 using the set and after that add {arr[i], i } 
+
+
+
+
+    return 0;
+}
